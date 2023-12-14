f5639ea8e9832ca6927ddeffc335cfc8164f9e27
madhukumarap <madhukumarap07@gmail.com>
2023-12-14 13:36:25 +0530

    first commit

---
+++ src/pages/HotDeals.js
@@ -0,0 +1,247 @@
+import styles from "./HotDeals.module.css";
+
+const HotDeals = () => {
+  return (
+    <div className={styles.hotDeals}>
+      <div className={styles.heading}>
+        <div className={styles.popularCategories}>Popular Products</div>
+        <div className={styles.button}>
+          <div className={styles.viewAll}>View All</div>
+          <img className={styles.groupIcon1} alt="" src="/group@2x.png" />
+        </div>
+      </div>
+      <div className={styles.product5n}>
+        <div className={styles.productImage}>
+          <img className={styles.imageIcon} alt="" src="/image@2x.png" />
+        </div>
+        <div className={styles.productInfo}>
+          <div className={styles.info}>
+            <div className={styles.greenApple}>Green Apple</div>
+            <div className={styles.price}>
+              <div className={styles.viewAll}>$14.99</div>
+              <div className={styles.div1}>$20.99</div>
+            </div>
+          </div>
+          <div className={styles.rating}>
+            <img className={styles.ratingChild} alt="" src="/star-1@2x.png" />
+            <img className={styles.ratingChild} alt="" src="/star-1@2x.png" />
+            <img className={styles.ratingChild} alt="" src="/star-1@2x.png" />
+            <img className={styles.ratingChild} alt="" src="/star-1@2x.png" />
+            <img className={styles.ratingChild} alt="" src="/star-5@2x.png" />
+          </div>
+        </div>
+        <img className={styles.addToCart10} alt="" src="/add-to-cart@2x.png" />
+        <div className={styles.tag}>
+          <div className={styles.sale}>Sale</div>
+          <div className={styles.viewAll}>50%</div>
+        </div>
+      </div>
+      <div className={styles.product5n1}>
+        <div className={styles.productImage}>
+          <img className={styles.imageIcon} alt="" src="/image@2x.png" />
+        </div>
+        <div className={styles.productInfo}>
+          <div className={styles.info}>
+            <div className={styles.greenApple}>Fresh Indian Malta</div>
+            <div className={styles.price1}>
+              <div className={styles.viewAll}>$20.00</div>
+            </div>
+          </div>
+          <div className={styles.rating}>
+            <img className={styles.ratingChild} alt="" src="/star-1@2x.png" />
+            <img className={styles.ratingChild} alt="" src="/star-1@2x.png" />
+            <img className={styles.ratingChild} alt="" src="/star-1@2x.png" />
+            <img className={styles.ratingChild} alt="" src="/star-1@2x.png" />
+            <img className={styles.ratingChild} alt="" src="/star-5@2x.png" />
+          </div>
+        </div>
+        <img className={styles.addToCart10} alt="" src="/add-to-cart@2x.png" />
+      </div>
+      <div className={styles.product5n2}>
+        <div className={styles.productImage}>
+          <img className={styles.imageIcon} alt="" src="/image@2x.png" />
+        </div>
+        <div className={styles.productInfo}>
+          <div className={styles.info}>
+            <div className={styles.greenApple}>Corn</div>
+            <div className={styles.price1}>
+              <div className={styles.viewAll}>$20.00</div>
+            </div>
+          </div>
+          <div className={styles.rating}>
+            <img className={styles.ratingChild} alt="" src="/star-1@2x.png" />
+            <img className={styles.ratingChild} alt="" src="/star-1@2x.png" />
+            <img className={styles.ratingChild} alt="" src="/star-1@2x.png" />
+            <img className={styles.ratingChild} alt="" src="/star-1@2x.png" />
+            <img className={styles.ratingChild} alt="" src="/star-5@2x.png" />
+          </div>
+        </div>
+        <img className={styles.addToCart10} alt="" src="/add-to-cart@2x.png" />
+      </div>
+      <div className={styles.product5n3}>
+        <div className={styles.productImage}>
+          <img className={styles.imageIcon} alt="" src="/image@2x.png" />
+        </div>
+        <div className={styles.productInfo}>
+          <div className={styles.info}>
+            <div className={styles.greenApple}>Big Potatoes</div>
+            <div className={styles.price1}>
+              <div className={styles.viewAll}>$20.00</div>
+            </div>
+          </div>
+          <div className={styles.rating}>
+            <img className={styles.ratingChild} alt="" src="/star-1@2x.png" />
+            <img className={styles.ratingChild} alt="" src="/star-1@2x.png" />
+            <img className={styles.ratingChild} alt="" src="/star-1@2x.png" />
+            <img className={styles.ratingChild} alt="" src="/star-1@2x.png" />
+            <img className={styles.ratingChild} alt="" src="/star-5@2x.png" />
+          </div>
+        </div>
+        <img className={styles.addToCart10} alt="" src="/add-to-cart@2x.png" />
+      </div>
+      <div className={styles.product5n4}>
+        <div className={styles.productImage}>
+          <img className={styles.imageIcon} alt="" src="/image@2x.png" />
+        </div>
+        <div className={styles.productInfo}>
+          <div className={styles.info}>
+            <div className={styles.greenApple}>Fresh Cauliflower</div>
+            <div className={styles.price1}>
+              <div className={styles.viewAll}>$12.00</div>
+            </div>
+          </div>
+          <div className={styles.rating}>
+            <img className={styles.ratingChild} alt="" src="/star-1@2x.png" />
+            <img className={styles.ratingChild} alt="" src="/star-1@2x.png" />
+            <img className={styles.ratingChild} alt="" src="/star-1@2x.png" />
+            <img className={styles.ratingChild} alt="" src="/star-1@2x.png" />
+            <img className={styles.ratingChild} alt="" src="/star-5@2x.png" />
+          </div>
+        </div>
+        <img className={styles.addToCart10} alt="" src="/add-to-cart@2x.png" />
+      </div>
+      <div className={styles.product5n5}>
+        <div className={styles.productImage}>
+          <img className={styles.imageIcon} alt="" src="/image@2x.png" />
+        </div>
+        <div className={styles.productInfo}>
+          <div className={styles.info}>
+            <div className={styles.greenApple}>Green Lettuce</div>
+            <div className={styles.price1}>
+              <div className={styles.viewAll}>$9.00</div>
+            </div>
+          </div>
+          <div className={styles.rating}>
+            <img className={styles.ratingChild} alt="" src="/star-1@2x.png" />
+            <img className={styles.ratingChild} alt="" src="/star-1@2x.png" />
+            <img className={styles.ratingChild} alt="" src="/star-1@2x.png" />
+            <img className={styles.ratingChild} alt="" src="/star-1@2x.png" />
+            <img className={styles.ratingChild} alt="" src="/star-5@2x.png" />
+          </div>
+        </div>
+        <img className={styles.addToCart10} alt="" src="/add-to-cart@2x.png" />
+      </div>
+      <div className={styles.product5n6}>
+        <div className={styles.productImage}>
+          <img className={styles.imageIcon} alt="" src="/image@2x.png" />
+        </div>
+        <div className={styles.productInfo}>
+          <div className={styles.info}>
+            <div className={styles.greenApple}>Green Capsicum</div>
+            <div className={styles.price}>
+              <div className={styles.viewAll}>$9.00</div>
+              <div className={styles.div1}>$20.99</div>
+            </div>
+          </div>
+          <div className={styles.rating}>
+            <img className={styles.ratingChild} alt="" src="/star-1@2x.png" />
+            <img className={styles.ratingChild} alt="" src="/star-1@2x.png" />
+            <img className={styles.ratingChild} alt="" src="/star-1@2x.png" />
+            <img className={styles.ratingChild} alt="" src="/star-1@2x.png" />
+            <img className={styles.ratingChild} alt="" src="/star-5@2x.png" />
+          </div>
+        </div>
+        <img className={styles.addToCart10} alt="" src="/add-to-cart@2x.png" />
+        <div className={styles.tag}>
+          <div className={styles.sale}>Sale</div>
+          <div className={styles.viewAll}>50%</div>
+        </div>
+      </div>
+      <div className={styles.product5n7}>
+        <div className={styles.productImage}>
+          <img className={styles.imageIcon} alt="" src="/image@2x.png" />
+        </div>
+        <div className={styles.productInfo}>
+          <div className={styles.info}>
+            <div className={styles.greenApple}>Eggplant</div>
+            <div className={styles.price1}>
+              <div className={styles.viewAll}>$34.00</div>
+            </div>
+          </div>
+          <div className={styles.rating}>
+            <img className={styles.ratingChild} alt="" src="/star-1@2x.png" />
+            <img className={styles.ratingChild} alt="" src="/star-1@2x.png" />
+            <img className={styles.ratingChild} alt="" src="/star-1@2x.png" />
+            <img className={styles.ratingChild} alt="" src="/star-1@2x.png" />
+            <img className={styles.ratingChild} alt="" src="/star-5@2x.png" />
+          </div>
+        </div>
+        <img className={styles.addToCart10} alt="" src="/add-to-cart@2x.png" />
+      </div>
+      <div className={styles.product5n8}>
+        <div className={styles.productImage}>
+          <img className={styles.imageIcon} alt="" src="/image@2x.png" />
+        </div>
+        <div className={styles.productInfo}>
+          <div className={styles.info}>
+            <div className={styles.greenApple}>Green Chili</div>
+            <div className={styles.price1}>
+              <div className={styles.viewAll}>$34.00</div>
+            </div>
+          </div>
+          <div className={styles.rating}>
+            <img className={styles.ratingChild} alt="" src="/star-1@2x.png" />
+            <img className={styles.ratingChild} alt="" src="/star-1@2x.png" />
+            <img className={styles.ratingChild} alt="" src="/star-1@2x.png" />
+            <img className={styles.ratingChild} alt="" src="/star-1@2x.png" />
+            <img className={styles.ratingChild} alt="" src="/star-5@2x.png" />
+          </div>
+        </div>
+        <img className={styles.addToCart10} alt="" src="/add-to-cart@2x.png" />
+      </div>
+      <div className={styles.product5n9}>
+        <div className={styles.productImage}>
+          <img className={styles.imageIcon} alt="" src="/image@2x.png" />
+        </div>
+        <div className={styles.productInfo}>
+          <div className={styles.info}>
+            <div className={styles.greenApple}>Chinese cabbage</div>
+            <div className={styles.price1}>
+              <div className={styles.viewAll}>$12.00</div>
+            </div>
+          </div>
+          <div className={styles.rating}>
+            <img className={styles.ratingChild} alt="" src="/star-1@2x.png" />
+            <img className={styles.ratingChild} alt="" src="/star-1@2x.png" />
+            <img className={styles.ratingChild} alt="" src="/star-1@2x.png" />
+            <img className={styles.ratingChild} alt="" src="/star-1@2x.png" />
+            <img className={styles.ratingChild} alt="" src="/star-5@2x.png" />
+          </div>
+        </div>
+        <img
+          className={styles.addToWishlist1}
+          alt=""
+          src="/add-to-wishlist@2x.png"
+        />
+        <img
+          className={styles.quickViewIcon1}
+          alt=""
+          src="/quick-view@2x.png"
+        />
+        <img className={styles.addToCart10} alt="" src="/add-to-cart@2x.png" />
+      </div>
+    </div>
+  );
+};
+
+export default HotDeals;
